284377416b356afa94c948988d6e7dc3b7b06ce9
Steven McKeown <stevenmckeown19@gmail.com>
2022-08-30 22:47:01 +0100

    Updated MovieCard component to use MaterialUI Card

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,16 +1,27 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+
 const MovieCard = ({ title, poster }) => {
   return (
-    <div>
-      <p>{title}</p>
-      <img
-        src={
-          poster
-            ? `https://image.tmdb.org/t/p/w500${poster}`
-            : console.log("error")
-        }
-        alt={`Poster for ${title}`}
-      />
-    </div>
+    <Card sx={{ maxWidth: 200 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="266"
+          image={`https://image.tmdb.org/t/p/w500${poster}`}
+          alt={title}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h6" component="div">
+            {title}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
   );
 };
 
