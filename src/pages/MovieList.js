0891f7bb401e881b23d28e3a1a8ff975781fec60
Steven McKeown <stevenmckeown19@gmail.com>
2022-08-31 13:17:47 +0100

    Added dynamic movie list choice

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,20 +1,48 @@
 import { useEffect, useState } from "react";
-import { getPopularMovies } from "../api/tmdb";
+import {
+  getPopularMovies,
+  getTopRatedMovies,
+  getUpcomingMovies,
+} from "../api/tmdb";
 import MovieCard from "../components/MovieCard";
 import styles from "./MovieList.module.css";
 
 const MovieList = () => {
   const [movies, setMovies] = useState([]);
+  const [listChoice, setListChoice] = useState(getPopularMovies);
 
   useEffect(() => {
-    getPopularMovies().then((movieData) => {
+    listChoice.then((movieData) => {
       setMovies([...movieData]);
     });
-  }, []);
+  }, [listChoice]);
   console.log(movies);
+
+  const handleListChoice = (e) => {
+    e.preventDefault();
+    switch (e.target.value) {
+      case "popular":
+        setListChoice(getPopularMovies);
+        break;
+      case "top_rated":
+        setListChoice(getTopRatedMovies);
+        break;
+      case "upcoming":
+        setListChoice(getUpcomingMovies);
+        break;
+      default:
+        setListChoice(getPopularMovies);
+        break;
+    }
+  };
 
   return (
     <div>
+      <select name="list-select" id="list-select" onClick={handleListChoice}>
+        <option value="popular">Popular</option>
+        <option value="top_rated">Top Rated</option>
+        <option value="upcoming">Upcoming</option>
+      </select>
       <ul className={styles.layout}>
         {movies.map((movie) => {
           return <MovieCard title={movie.title} poster={movie.poster_path} />;
