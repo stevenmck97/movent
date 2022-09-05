2315b460e6942dcb3680b953c372d9828b0971d5
Steven McKeown <stevenmckeown19@gmail.com>
2022-09-05 15:11:11 +0100

    Added get and post functions for favourites list on client side

---
+++ client/src/api/server.js
@@ -0,0 +1,27 @@
+export const getFavouritesList = async () => {
+  try {
+    const res = await fetch("http://localhost:5000/favourites", {
+      method: "GET",
+    });
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const addFavourite = async (movie) => {
+  try {
+    const res = await fetch("http://localhost:5000/favourites", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(movie),
+    });
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
