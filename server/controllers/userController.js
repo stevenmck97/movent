1f365ec80243595860bd36944635ae1355a96354
Steven McKeown <stevenmckeown19@gmail.com>
2022-09-07 17:03:15 +0100

    Updated registerUser controller to hash req password and add it to db. Added loginUser controller to check if the user exists in db and that their passwords match

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,48 @@
-const Users = require('../models/userModel');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
 
-exports.registerUser = function(req, res) {
-    const { email, password, name } = req.body;
+exports.registerUser = asyncHandler(async function (req, res) {
+  const { email, password, name } = req.body;
 
-    const user = await Users.create({ email, password, name });
-};
+  if (!email || !password || !name) {
+    res.status(400);
+    throw new Error("Please fill in all fields");
+  }
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const user = await User.create({ email, password: hashedPassword, name });
+  if (user) {
+    res.status(201);
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
+exports.loginUser = asyncHandler(async function (req, res) {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (user && isMatch) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+});
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,8 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const userController = require("../controllers/userController");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
+router.post("/register", userController.registerUser);
+// router.post("/login", userController.loginUser);
 module.exports = router;
