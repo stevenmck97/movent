fe9c97054f34665c460b7dc28ad6dacec6815268
Steven McKeown <stevenmckeown19@gmail.com>
2022-09-08 17:18:48 +0100

    Updated favourites route to be protected

--- server/controllers/favouritesController.js
+++ server/controllers/favouritesController.js
@@ -1,7 +1,9 @@
 const Favourites = require("../models/favourites");
+const User = require("../models/userModel");
+const asyncHandler = require("express-async-handler");
 
 exports.favourites_list = function (req, res, next) {
-  Favourites.find().exec(function (err, list_favourites) {
+  Favourites.find({ user: req.user.id }).exec(function (err, list_favourites) {
     if (err) {
       return next(err);
     }
@@ -12,6 +14,7 @@ exports.favourites_list = function (req, res, next) {
 exports.favourite_create = function (req, res, next) {
   //might need to be array
   const favourite = new Favourites({
+    user: req.user.id,
     favourite: { movie: req.body },
   });
 
@@ -23,7 +26,23 @@ exports.favourite_create = function (req, res, next) {
   });
 };
 
-exports.favourite_id = function (req, res, next) {
+exports.favourite_id = asyncHandler(async function (req, res, next) {
+  const user = await User.findById(req.user.id);
+  const favourite = await Favourites.findOne({
+    user: req.user.id,
+    "favourite.movie.id": parseInt(req.params.id),
+  });
+
+  if (!user) {
+    res.status(404);
+    throw new Error("User not found");
+  }
+
+  if (favourite.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("Not authorized");
+  }
+
   Favourites.find({ "favourite.movie.id": parseInt(req.params.id) }).exec(
     function (err, favourite) {
       if (err) {
@@ -32,9 +51,25 @@ exports.favourite_id = function (req, res, next) {
       res.json(favourite);
     }
   );
-};
+});
 
-exports.favourite_delete = function (req, res, next) {
+exports.favourite_delete = asyncHandler(async function (req, res, next) {
+  const user = await User.findById(req.user.id);
+  const favourite = await Favourites.findOne({
+    user: req.user.id,
+    "favourite.movie.id": parseInt(req.params.id),
+  });
+
+  if (!user) {
+    res.status(404);
+    throw new Error("User not found");
+  }
+
+  if (favourite.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("Not authorized");
+  }
+
   Favourites.findOneAndDelete(
     { "favourite.movie.id": parseInt(req.params.id) },
     function (err, favourite) {
@@ -44,4 +79,4 @@ exports.favourite_delete = function (req, res, next) {
       res.json(favourite);
     }
   );
-};
+});
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,15 +1,20 @@
 var express = require("express");
 var router = express.Router();
 const favourites_controller = require("../controllers/favouritesController");
+const { protect } = require("../middleware/auth");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
-router.get("/favourites", favourites_controller.favourites_list);
-router.post("/favourites", favourites_controller.favourite_create);
-router.get("/favourites/:id", favourites_controller.favourite_id);
-router.delete("/favourites/:id", favourites_controller.favourite_delete);
+router.get("/favourites", protect, favourites_controller.favourites_list);
+router.post("/favourites", protect, favourites_controller.favourite_create);
+router.get("/favourites/:id", protect, favourites_controller.favourite_id);
+router.delete(
+  "/favourites/:id",
+  protect,
+  favourites_controller.favourite_delete
+);
 
 module.exports = router;
